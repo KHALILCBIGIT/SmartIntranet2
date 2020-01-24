cb2063b01933a488ca21c8e8f3ea6200902d4333
KhalilKertaoui <54622656+KhalilKertaoui@users.noreply.github.com>
2020-01-24 16:09:04 +0100

    AAA

---
+++ SmartIntranet2/src/main/java/com/gestion/client/SmartIntranet2Application.java
@@ -0,0 +1,13 @@
+package com.gestion.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartIntranet2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartIntranet2Application.class, args);
+	}
+
+}
---
+++ SmartIntranet2/src/test/java/com/gestion/client/SmartIntranet2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartIntranet2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
